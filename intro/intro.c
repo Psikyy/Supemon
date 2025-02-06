70d9bf7a3f060a08c9a076c87ea3222e8e415899
Arthur <arthur.omar@supinfo.com>
2025-02-06 15:28:01 +0100

    Changement de l'intro et du main.c

--- intro/intro.c
+++ intro/intro.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "intro.h"
 
 int intro(char *nom) {
@@ -12,30 +13,29 @@ int intro(char *nom) {
     printf("|Choose your starter Sup%cmon: |\n", é);
     printf("|\t1 - Supmander         |\n");
     printf("|\t2 - Supasaur          |\n");
-    printf("|\t3 - Supmander         |\n");
+    printf("|\t3 - Supirtle          |\n");
     printf("+-----------------------------+\n");
 
     printf("1, 2 or 3: ");
-    if (scanf("%d", &starter) != 1) {
-        printf("Erreur : entr%ce invalide !", é);
-        return 0;
-    }
-
-    if (starter < 1 || starter > 3) {
-        printf("Erreur : le starter n'existe pas !");
-        return 0;
+    
+    if (scanf("%d", &starter) != 1 || starter < 1 || starter > 3) {
+        printf("Entrée invalide ! Un Supémon aléatoire sera attribué.\n");
+        
+        srand(time(NULL));
+        starter = (rand() % 3) + 1;
     }
 
     switch (starter) {
         case 1:
-            printf("Vous avez choisi le starter num%cro 1, vous poss%cdez donc Supmander !\n", é, é);
+            printf("Vous avez choisi Supmander !\n");
             break;
         case 2:
-            printf("Vous avez choisi le starter num%cro 2, vous poss%cdez donc Supasaur !\n", é, é);
+            printf("Vous avez choisi Supasaur !\n");
             break;
         case 3:
-            printf("Vous avez choisi le starter num%cro 3, vous poss%cdez donc Supmander !\n", é, é);
+            printf("Vous avez choisi Supirtle !\n");
             break;
     }
+    
     return starter;
 }
--- supémon.c
+++ supémon.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "intro/intro.h"
+#include "class/player/player.h"
+#include "game/game_menu.h"
 
 int main() {
+    Player joueur;
     char nom[50];
 
-    printf("Veuilliez saisir votre nom : ");
+    printf("Veuillez saisir votre nom : ");
     scanf("%49s", nom);
 
-    intro(nom);
+    int starter_choice = intro(nom);
+    printf("Starter choisi : %d\n", starter_choice);
+
+    initialize_player(&joueur, nom, 500, starter_choice);
+    printf("Player initialisé, nom : %s\n", joueur.name);  // Vérifie si le nom est correctement affecté
+
+    int game_running = 1;
+    while (game_running) {
+        // Afficher menu et gérer les actions
+        game_running = game_menu(&joueur);
+    }
+
+    printf("Fin du jeu. Merci d'avoir joué !\n");
 
     return 0;
 }
