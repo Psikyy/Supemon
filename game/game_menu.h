163e51ddf91ef9207cd163c4dddfa5865d33b62a
Arthur <arthur.omar@supinfo.com>
2025-02-07 09:15:50 +0100

    Suppression du game_menu

--- game/game_menu.h
+++
@@ -1,9 +0,0 @@
-// game_menu.h
-#ifndef GAME_MENU_H
-#define GAME_MENU_H
-
-#include "../class/player/player.h"  // Inclure player.h avant la déclaration de game_menu
-
-int game_menu(Player *p);
-
-#endif
--- supémon.c
+++ supémon.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "intro/intro.h"
 #include "class/player/player.h"
-#include "game/game_menu.h"
 
 int main() {
     Player joueur;
@@ -16,13 +15,6 @@ int main() {
 
     initialize_player(&joueur, nom, 500, starter_choice);
     printf("Player initialisé, nom : %s\n", joueur.name);  // Vérifie si le nom est correctement affecté
-
-    int game_running = 1;
-    while (game_running) {
-        // Afficher menu et gérer les actions
-        game_running = game_menu(&joueur);
-    }
-
     printf("Fin du jeu. Merci d'avoir joué !\n");
 
     return 0;
