7dc42c7b81fca865d29f4e1179c997423d3105ad
Arthur <arthur.omar@supinfo.com>
2025-02-07 09:30:23 +0100

    Changement des classes

--- class/player/player.c
+++ class/player/player.c
@@ -1,18 +1,36 @@
-#include "Player.h"
+#include <stdio.h>
+#include <string.h>
+#include "player.h"
+#include "../supemon/supemon.h"
 
-void initialize_player(Player *p, const char *name, int supcoins) {
+void initialize_player(Player *p, const char *name, int supcoins, int starter_choice) {
     strcpy(p->name, name);
-    p->supemon_count = 0;
-    p->selected_supemon = NULL;
     p->supcoins = supcoins;
+    p->supemon_count = 1; // Un Supémon au départ
     p->item_count = 0;
+    p->selected_supemon = &p->supemons[0]; // On pointe vers le premier Supémon
+
+    // Initialisation des mouvements
+    Move empty_moves[MAX_MOVES] = { { "", 0, 0, "" }, { "", 0, 0, "" } };
+
+    // Choisir le Supémon de départ
+    Supemon starter;
+    switch (starter_choice) {
+        case 1:
+            initialize_supemon(&starter, "Supmander", 10, 1, 1, 1, 2, 1, empty_moves); break;
+        case 2:
+            initialize_supemon(&starter, "Supasaur", 9, 1, 1, 3, 2, 2, empty_moves); break;
+        case 3:
+            initialize_supemon(&starter, "Supirtle", 11, 1, 2, 2, 1, 2, empty_moves); break;
+        default:
+            printf("Choix invalide pour le Supémon de départ.\n");
+            return;
+    }
+    p->supemons[0] = starter; // Affecte le Supémon au joueur
 }
 
 void add_supemon(Player *p, Supemon s) {
     if (p->supemon_count < MAX_SUPEMONS) {
         p->supemons[p->supemon_count++] = s;
-        if (p->selected_supemon == NULL) {
-            p->selected_supemon = &p->supemons[0];
-        }
     }
 }
--- class/supemon/supemon.c
+++ class/supemon/supemon.c
@@ -1,17 +1,17 @@
-#include "Supemon.h"
+#include <stdio.h>
+#include <string.h>
+#include "supemon.h"
+#include "../player/player.h"
 
 void initialize_supemon(Supemon *s, const char *name, int hp, int attack, int defense, int evasion, int accuracy, int speed, Move moves[]) {
     strcpy(s->name, name);
     s->level = 1;
     s->experience = 0;
-    s->max_hp = hp;
-    s->hp = hp;
-    s->base_attack = s->attack = attack;
-    s->base_defense = s->defense = defense;
-    s->base_evasion = s->evasion = evasion;
-    s->base_accuracy = s->accuracy = accuracy;
+    s->hp = s->max_hp = hp;
+    s->attack = s->base_attack = attack;
+    s->defense = s->base_defense = defense;
+    s->evasion = s->base_evasion = evasion;
+    s->accuracy = s->base_accuracy = accuracy;
     s->speed = speed;
-    for (int i = 0; i < MAX_MOVES; i++) {
-        s->moves[i] = moves[i];
-    }
+    memcpy(s->moves, moves, sizeof(Move) * MAX_MOVES);
 }
